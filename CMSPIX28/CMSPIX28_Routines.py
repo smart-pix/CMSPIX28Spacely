8626796a102d09a79639da19b822b946cedc9735
badea <badeaa3@gmail.com>
2024-04-12 10:21:03 -0500

    push new folder

---
+++ CMSPIX28/CMSPIX28_Routines.py
@@ -0,0 +1,22 @@
+#ExampleASIC Routines File
+#
+# This file contains any custom code that you want to write for your ASIC,
+# for example routines to read or write to registers, or to run tests.
+
+
+
+
+def ROUTINE_basicLoopback():
+    """This routine tests basic loopback from data_in to data_out"""
+
+    #Define the routine's purpose in a docstring like above, this will appear
+    #when you call the routine in Spacely.
+
+    pass
+
+
+
+
+# IMPORTANT! If you want to be able to run a routine easily from
+# spacely, put its name in the "ROUTINES" list:
+ROUTINES = [ROUTINE_basicLoopback]
