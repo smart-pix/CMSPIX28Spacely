6d6f3c2d86cf066453b0c6bee31da670e4eacdb9
badea <badeaa3@gmail.com>
2024-05-28 20:20:45 -0500

    working

--- SMARTPIX28/SMARTPIX28_Routines.py
+++ SMARTPIX28/SMARTPIX28_Routines.py
@@ -3,9 +3,13 @@
 # This file contains any custom code that you want to write for your ASIC,
 # for example routines to read or write to registers, or to run tests.
 
+# spacely functions
+from Master_Config import *
+import Spacely_Globals as sg
+from Spacely_Utils import *
 
 
-
+#<<Registered w/ Spacely as ROUTINE 0, call as ~r0>>
 def ROUTINE_basicLoopback():
     """This routine tests basic loopback from data_in to data_out"""
 
