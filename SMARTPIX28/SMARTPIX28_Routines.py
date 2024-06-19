7b84d95f81f38d5cb9434acaafbf97488886760d
Anthony Badea <abadea@fasic-137645.fnal.gov>
2024-06-19 17:05:18 -0500

    testing routines

--- SMARTPIX28/SMARTPIX28_Routines.py
+++ SMARTPIX28/SMARTPIX28_Routines.py
@@ -99,23 +99,57 @@ def ROUTINE_basicLoopback():
     pass
 
 
-#<<Registered w/ Spacely as ROUTINE 1, call as ~r1>>
-def ROUTINE_clk_divide():
+# #<<Registered w/ Spacely as ROUTINE 1, call as ~r1>>
+# def ROUTINE_clk_divide():
     
+#     # check register initial value and store it
+#     sw_write32_0 = sg.INSTR["car"].get_memory("sw_write32_0")
+#     print(f"Starting register value sw_write32_0 = {sw_write32_0}")
+#     sw_write32_0_init = sw_write32_0
+    
+#     # loop over clk_divides from 10 to 40
+#     for divide in range(10, 41):
+      
+#         # get clk_divide value
+#         clk_divide = "6'h" + hex(divide).strip("0x")
+
+#         # create hex list
+#         hex_list = ["4'h2", "4'h2", "11'h0", "1'h0", "1'h0", "5'h4", clk_divide]
+
+#         # convert hex list to input to set memory
+#         temp = gen_sw_write32_0(hex_list)
+
+#         # do write
+#         sw_write32_0 = sg.INSTR["car"].set_memory("sw_write32_0", temp)
+
+#         # read back
+#         sw_write32_0 = sg.INSTR["car"].get_memory("sw_write32_0")
+#         print(f"Wrote {temp} and register reads {sw_write32_0}. clk_divide = {divide} ({clk_divide})")
+
+#         # sleep between consecutive writes
+#         time.sleep(1)  
+
+#     print(f"Returning register to how it started sw_write32_0 = {sw_write32_0_init}")
+#     sw_write32_0 = sg.INSTR["car"].set_memory("sw_write32_0", sw_write32_0_init)
+#     sw_write32_0 = sg.INSTR["car"].get_memory("sw_write32_0")
+#     print(f"Register returned to initial value: {sw_write32_0_init == sw_write32_0}")
+
+
+#<<Registered w/ Spacely as ROUTINE 1, call as ~r1>>
+def ROUTINE_sw_write32_0(hex_lists = None):
+
+    # check if hex_lists is none. if so then put a default value in
+    if hex_lists == None:
+        hex_lists = [ ["4'h2", "4'h2", "11'h0", "1'h0", "1'h0", "5'h4", "6'ha"] ]
+
     # check register initial value and store it
     sw_write32_0 = sg.INSTR["car"].get_memory("sw_write32_0")
     print(f"Starting register value sw_write32_0 = {sw_write32_0}")
     sw_write32_0_init = sw_write32_0
-    
-    # loop over clk_divides from 10 to 40
-    for divide in range(10, 41):
-      
-        # get clk_divide value
-        clk_divide = "6'h" + hex(divide).strip("0x")
 
-        # create hex list
-        hex_list = ["4'h2", "4'h2", "11'h0", "1'h0", "1'h0", "5'h4", clk_divide]
-
+    # loop over the write values
+    for hex_list in hex_lists:
+        
         # convert hex list to input to set memory
         temp = gen_sw_write32_0(hex_list)
 
@@ -124,15 +158,74 @@ def ROUTINE_clk_divide():
 
         # read back
         sw_write32_0 = sg.INSTR["car"].get_memory("sw_write32_0")
-        print(f"Wrote {temp} and register reads {sw_write32_0}. clk_divide = {divide} ({clk_divide})")
+        print(f"Wrote {temp} and register reads {sw_write32_0}. hex_list = {hex_list}")
 
         # sleep between consecutive writes
-        time.sleep(1)  
+        time.sleep(1)
 
     print(f"Returning register to how it started sw_write32_0 = {sw_write32_0_init}")
     sw_write32_0 = sg.INSTR["car"].set_memory("sw_write32_0", sw_write32_0_init)
     sw_write32_0 = sg.INSTR["car"].get_memory("sw_write32_0")
     print(f"Register returned to initial value: {sw_write32_0_init == sw_write32_0}")
+
+
+#<<Registered w/ Spacely as ROUTINE 2, call as ~r2>>
+def ROUTINE_clk_divide():
+
+    # create hex lists
+    hex_lists = []
+
+    # 1st parameter of the hex code (right to left)
+    # this number divides the original 400 MHz down for the bclk and bclk_ana
+    # 400 MHz / clk_divide
+    clk_divides = range(10, 41)
+
+    # loop over clk_divides from 10 to 40
+    for divide in clk_divides:
+        # get clk_divide value
+        clk_divide = "6'h" + hex(divide)[2:] # removes the 0x prefix, ex. 0x28 -> 28
+        # create hex list
+        hex_list = ["4'h2", "4'h2", "11'h0", "1'h0", "1'h0", "5'h4", clk_divide]
+        hex_lists.append(hex_list)
+
+    print(f"Writing {len(hex_lists)} hex lists to register sw_write32_0")
+
+    # call ROUTINE_sw_write32_0
+    ROUTINE_sw_write32_0(hex_lists)
+
+
+#<<Registered w/ Spacely as ROUTINE 3, call as ~r3>>
+def ROUTINE_clk_delay():
+    
+    # create hex lists
+    hex_lists = []
+
+    # 2nd parameter of hex code (right to left)
+    # change the amount of the delay
+    delays = range(0,6)
+    
+    # 3rd parameter of hex code (right to left)
+    # change the sign of the delay
+    # 1'h0 = positive offset of analog clock w.r.t digital clock = rising edge of analog clock and the rising egde of the digital clock
+    # 1'h1 = negative offset of analog clock w.r.t digital clock = rising edge of analog clock and the falling edge of the digital clock
+    signs = ["1'h0", "1'h1"]
+
+    # create hex list to be easy to see on the scope
+    for sign in signs:
+
+        for delay in delays:
+
+            # get clk_divide value
+            clk_delay = "5'h" + hex(delay)[2:] # removes the 0x prefix, ex. 0x28 -> 28
+
+            # create hex list
+            hex_list = ["4'h2", "4'h2", "11'h0", "1'h0", sign, clk_delay, "6'h28"]
+            hex_lists.append(hex_list)
+        
+    print(f"Writing {len(hex_lists)} hex lists to register sw_write32_0")
+
+    # call ROUTINE_sw_write32_0
+    ROUTINE_sw_write32_0(hex_lists)
 
 # IMPORTANT! If you want to be able to run a routine easily from
 # spacely, put its name in the "ROUTINES" list:
