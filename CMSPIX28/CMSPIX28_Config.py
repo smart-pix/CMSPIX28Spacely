c92a82003b104a79f80906cc6a63c845fbff11d7
Anthony Badea <abadea@fasic-137645.fnal.gov>
2024-06-25 15:46:43 -0500

    rename peary device to CMSPIX28

--- CMSPIX28/CMSPIX28_Config.py
+++ CMSPIX28/CMSPIX28_Config.py
@@ -3,4 +3,4 @@
 INSTR = {"car" : {"type": "Caribou",
                   "host":"192.168.1.24", #"192.168.7.63",
                   "port":12345,
-                  "device":"SMARTPIX28"}}
+                  "device":"CMSPIX28"}}
