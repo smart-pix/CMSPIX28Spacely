2a962d0c9bc2a5479f5a8ad363961d782963733b
Anthony Badea <abadea@fasic-137645.fnal.gov>
2024-08-16 06:42:51 -0500

    Move helper functions into a Subroutines file. Master_Config.py automatically imports this file

--- CMSPIX28/CMSPIX28_Routines.py
+++ CMSPIX28/CMSPIX28_Routines.py
@@ -1,51 +1,17 @@
-'''
-Author: Anthony Badea
+B'''
+Authors: Anthony Badea, Benjamin Parpillon
 Date: June, 2024
 '''
 
 # python
 import time
 
-# spacely
+#spacely
 from Master_Config import *
 import Spacely_Globals as sg
 from Spacely_Utils import *
 
-# Function to convert each hex value to a binary string with proper bit width
-def hex_to_bin(hex_str):
-    bit_width, hex_value = hex_str.split("'h") # get the bit length and hex value
-    bit_width = int(bit_width) # convert bit length to an int
-    decimal_value = int(hex_value, 16) # Convert the hexadecimal number to an integer
-    binary_str = bin(decimal_value)[2:].zfill(bit_width) # Convert the hexadecimal number to an integer
-    return binary_str
-
-def gen_sw_write32_0(hex_list):
-    # Convert the list of hex values to a single binary string
-    binary_str = ''.join(hex_to_bin(hex_str) for hex_str in hex_list)
-    # Convert the binary string to an integer
-    resulting_int = int(binary_str, 2)
-    # return
-    # print(binary_str, resulting_int)
-    return resulting_int
-
-def int_to_32bit_hex(number):
-    # Ensure the number is treated as a 32-bit number
-    # by masking with 0xFFFFFFFF
-    hex_number = format(number & 0xFFFFFFFF, '08x')
-    return hex_number
-
-def int_to_32bit(number):
-    return format(number & 0xFFFFFFFF, '032b')
-
-def print_test_header(word, div="*"):
-    print(word)
-    print(div*len(word))
-
-def print_test_footer(PASS):
-    print("****")
-    print("Test result:", "Pass" if PASS else "Fail")
-    print("****")
-    print("\n")
+# note that all functions in CMSPIX28_Subroutines.py will automatically be imported by Master_Config.py
 
 #<<Registered w/ Spacely as ROUTINE 0, call as ~r0>>
 def ROUTINE_sw_write32_0(
