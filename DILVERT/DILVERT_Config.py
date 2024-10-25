bbee3788370d94dd10e4072ea965877fa39bdb09
aquinnfnal <115590933+aquinnfnal@users.noreply.github.com>
2024-10-25 17:07:13 -0500

    DILVERT SC Loopback Working

--- DILVERT/DILVERT_Config.py
+++ DILVERT/DILVERT_Config.py
@@ -4,6 +4,7 @@
 # your ASIC test stand is set up, for example power supply voltages.
 import os
 
+USE_NI = True
 
 DEFAULT_FPGA_BITFILE_MAP = {"PXI1Slot6":"NI7976_NI6583_40MHz"}
 
@@ -42,15 +43,17 @@ INSTR = {"SMU_A" : {"type" : "NIDCPower",
 
 # Example: We have two voltage rails, VDD and Vref. VDD will be initialized
 #          first. We have one current rail, Ibias.
-V_SEQUENCE = ["VDDIO", "VDDC", "VTUNE_N_FAST", "VTUNE_P_FAST", "VTUNE_N_SLOW", "VTUNE_P_SLOW", 
+V_SEQUENCE = ["LS_3V3","VDDIO", "VDDC", "VTUNE_N_FAST", "VTUNE_P_FAST", "VTUNE_N_SLOW", "VTUNE_P_SLOW", 
                "VTUNE_N_RO", "VTUNE_P_RO", "NMOS_BG_BIAS", "PMOS_BG_BIAS"]
 I_SEQUENCE = []
 
+REVERSE_FLANGE = False
 
 # 2) Define the instrument + channel that will be used for each rail:
 
 # Example: VDD is on myPSU channel 0, etc.
-V_INSTR = {"VDDIO": "PSU_A",
+V_INSTR = {"LS_3V3":"PSU_A",
+           "VDDIO": "PSU_A",
            "VDDC": "PSU_B",
            "VTUNE_N_FAST":"SMU_A",
            "VTUNE_P_FAST":"SMU_A",
@@ -60,7 +63,8 @@ V_INSTR = {"VDDIO": "PSU_A",
            "VTUNE_P_RO"  :"SMU_A",
            "NMOS_BG_BIAS":"SMU_B",
            "PMOS_BG_BIAS":"SMU_A"}
-V_CHAN  = {"VDDIO": 0,
+V_CHAN  = {"LS_3V3":0,
+           "VDDIO": 1,
            "VDDC": 0,
            "VTUNE_N_FAST":3,
            "VTUNE_P_FAST":2,
@@ -76,13 +80,14 @@ I_CHAN  = {}
 
 # 3) Define the nominal output level. V_PORTs are in volts, I_PORTs are in amps.
 
-V_LEVEL = {"VDDIO": 1.8,
+V_LEVEL = {"LS_3V3":3.3,
+           "VDDIO": 1.8,
            "VDDC": 0.8,
-           "VTUNE_N_FAST":1.9,
-           "VTUNE_P_FAST":0.1,
-           "VTUNE_N_SLOW":0.55,
+           "VTUNE_N_FAST": 1.9,
+           "VTUNE_P_FAST": 0.1,
+           "VTUNE_N_SLOW": 0.55,
            "VTUNE_P_SLOW":1.45,
-           "VTUNE_N_RO"  :1,
+           "VTUNE_N_RO"  :  1,
            "VTUNE_P_RO"  :1,
            "NMOS_BG_BIAS":1,
            "PMOS_BG_BIAS":-1}     
@@ -91,7 +96,9 @@ I_LEVEL = {} #100 uA
 
 # 4) Define limit values. For V_PORTs we set a maximum current. For I_PORTs
 #    we set a maximum voltage.
-V_CURR_LIMIT = {"VDDIO": 0.1,
+V_CURR_LIMIT = {
+            "LS_3V3":0.5,
+            "VDDIO": 0.1,
            "VDDC": 0.1,
            "VTUNE_N_FAST":0.0001,
            "VTUNE_P_FAST":0.0001,
@@ -107,7 +114,8 @@ I_VOLT_LIMIT = {} #2.5 V
            
 # 5) Create V_PORT and I_PORT dictionaries. When Spacely initializes these rails,
 #    you will be able to access them with these dictionaries.
-V_PORT = {"VDDIO": None,
+V_PORT = {"LS_3V3":None,
+            "VDDIO": None,
            "VDDC": None,
            "VTUNE_N_FAST":None,
            "VTUNE_P_FAST":None,
@@ -122,7 +130,9 @@ I_PORT = {}
 # 6) (Optional) for each rail, define a warning voltage range in the
 #    form [Vhigh, Vlow]. Spacely will warn you if the voltage ever falls
 #    outside of this range.
-V_WARN_VOLTAGE = {"VDDIO": [1.7,1.9],
+V_WARN_VOLTAGE = {
+            "LS_3V3":[3.2,3.4],
+            "VDDIO": [1.7,1.9],
            "VDDC": [0.7,0.9],
            "VTUNE_N_FAST":[0,2.0],
            "VTUNE_P_FAST":[0,2.0],
@@ -135,4 +145,15 @@ V_WARN_VOLTAGE = {"VDDIO": [1.7,1.9],
 I_WARN_VOLTAGE = {}
 
 
+if REVERSE_FLANGE:
+    print("!!! WARNING -- Config file is written to use a CRYO_ISOk63_FLANGE_ANALOG Board with REVERSED connectors!!!")
+    
+    for rail in V_SEQUENCE:
+        if "SMU" in V_INSTR[rail]:
+            #Swap channels 0~3
+            V_CHAN[rail] = 3 - V_CHAN[rail]
 
+            #Swap polarities of warn and set voltages:
+            V_LEVEL[rail] = -1*V_LEVEL[rail]
+            V_WARN_VOLTAGE[rail][0] = -1*V_WARN_VOLTAGE[rail][0]
+            V_WARN_VOLTAGE[rail][1] = -1*V_WARN_VOLTAGE[rail][1]
--- DILVERT/DILVERT_Routines.py
+++ DILVERT/DILVERT_Routines.py
@@ -9,20 +9,151 @@ from Master_Config import *
 import Spacely_Globals as sg
 from Spacely_Utils import *
 
-#<<Registered w/ Spacely as ROUTINE 0, call as ~r0>>
-def ROUTINE_congrats():
-    """If you can run this routine from the Spacely command line ('~r0'), you've installed everything correctly!"""
-    
-    sg.log.notice(" `*%*` CONGRATS! `*%*` (Spacely is installed correctly.) ")
 
+
+DEFAULT_SC = None
+
+def onstartup():
+    global DEFAULT_SC
+    DEFAULT_SC = DILVERT_SC([0]*25)
+    
+    DEFAULT_SC.set_field("DOUT1_Select",1)
+    DEFAULT_SC.set_field("DOUT2_Select",1)
+    DEFAULT_SC.set_field("CAL1",1)
+    DEFAULT_SC.set_field("DRV",3)
+
+    DEFAULT_SC.print_fields()
+
+
+
+
+#<<Registered w/ Spacely as ROUTINE 0, call as ~r0>>
+def ROUTINE_check_sc_loopback():
+   
+   pulse_reset()
+   
+   print("Testing loopback w/o pass/load")
+   
+   serial_write(DEFAULT_SC, s_pass=False)
+   return_sc = serial_read(s_load = False)
+   
+   return_sc.print_fields()
+   
+   if DEFAULT_SC == return_sc:
+       print("PASS!")
+   else:
+       print("FAIL!")
+   
+   print("Testing loopback w/ pass/load")
+   
+   serial_write(DEFAULT_SC, s_pass=True)
+   return_sc = serial_read(s_load = True)
+   
+   return_sc.print_fields()
+   
+   SC2 = DILVERT_SC([0]*25)
+    
+   SC2.set_field("DOUT1_Select",1)
+   SC2.set_field("DOUT2_Select",1)
+   SC2.set_field("CAL1",1)
+   SC2.set_field("DRV",3)
+   SC2.set_field("QFINE",99)
+   
+   if SC2 == return_sc:
+       print("PASS!")
+   else:
+       print("FAIL!")
+   
 
 #<<Registered w/ Spacely as ROUTINE 1, call as ~r1>>
-def ROUTINE_basicLoopback():
-    """This routine tests basic loopback from data_in to data_out"""
+def ROUTINE_get_avg_core_power():
+    """Returns the average power consumed by VDDC over 100 queries."""
+    
+    power_results = []
+    
+    for _ in range(100):
+        power_results.append(V_PORT["VDDC"].get_current()*0.8)
+        
+    print(f"Minimum power: {min(power_results)}")
+    print(f"Maximum power: {max(power_results)}")
+    print(f"\nAVERAGE POWER: {sum(power_results)/100}")
 
-    #Define the routine's purpose in a docstring like above, this will appear
-    #when you call the routine in Spacely.
 
-    pass
-
-
+#<<Registered w/ Spacely as ROUTINE 2, call as ~r2>>
+def ROUTINE_tuning_sensitivity_analysis():
+    """Analyze the sensitivity of resolution to tuning voltage."""
+    
+    # Because setting the three pulse intervals requires human intervention,
+    # we are going to collect all the necessary tuning voltage points for 
+    # each power rail at a time.
+    
+    PULSE_TIME_STEPS = [0,100,1000]
+    
+    V_SLOW_CHAIN_NOM = 0.55
+    V_SLOW_CHAIN_RANGE = [x/100 for x in range(0,200)]
+    
+    V_RO_NOM = 1
+    V_RO_RANGE = [x/100 for x in range(0,200)]
+    
+    TEST_V_RO = [V_RO_NOM]*len(V_SLOW_CHAIN_RANGE) + V_RO_RANGE
+    TEST_V_SLOW_CHAIN = V_SLOW_CHAIN_RANGE + [V_SLOW_CHAIN_NOM]*len(V_RO_RANGE)
+    
+    TEST_N = len(TEST_V_RO)
+    assert(TEST_N == len(TEST_V_SLOW_CHAIN))
+    
+    sg.log.debug(f"Beginning tuning sensitivity analysis. Will gather {TEST_N} voltage points.")
+    
+    ## STAGE 1: Data Collection
+    
+    #Each key of the QCOARSE/QFINE dictionaries is a pulse time. 
+    #Each value is a list of the Qc/Qf values for each voltage condition under that pulse time.
+    QCOARSE = {}
+    QFINE   = {}
+    
+    for pulse_time in PULSE_TIME_STEPS:
+        
+        QCOARSE[pulse_time] = []
+        QFINE[pulse_time] = []
+        
+        input(f"Please configure the pulse generator for delT={pulse_time} picoseconds and press 'Enter' when ready.")
+        
+        for i in range(TEST_N):
+            sg.log.debug(f"Test Condition {i}: V_RO={TEST_V_RO[i]} V_SLOW_CHAIN={TEST_V_SLOW_CHAIN[i]}")
+            
+            V_PORT["VTUNE_N_RO"].set_voltage(TEST_V_RO[i])
+            V_PORT["VTUNE_P_RO"].set_voltage(2-TEST_V_RO[i])
+            V_PORT["VTUNE_N_SLOW"].set_voltage(TEST_V_SLOW_CHAIN[i])
+            V_PORT["VTUNE_P_SLOW"].set_voltage(2-TEST_V_SLOW_CHAIN[i])
+            
+            time.sleep(0.001)
+            
+            (Qc, Qf) = get_TDC_result(N_avg=100)
+        
+            QCOARSE[pulse_time].append(Qc)
+            QFINE[pulse_time].append(Qf)
+    
+    
+    ## STAGE 2: Analysis
+    
+    print("TBD!!! Implement system of equation solving")
+        
+def get_TDC_result(N_avg = 1):
+    """Gets 1 or more TDC results and averages them together"""
+    
+    Qc = []
+    Qf = []
+    
+    for i in range(N_avg):
+        # (1) Wait for the rising edge of START/STOP (TRIGGER)
+    
+        print("ERROR! NEED TO IMPLEMENT WAIT FOR TRIGGER")
+    
+        # (2) Grab data from the TDC internal registers.
+        sc_contents = serial_read()
+    
+        pulse_reset()
+        
+        Qc.append(sc_contents.get_field("QCOARSE"))
+        Qf.append(sc_contents.get_field("QFINE"))
+        
+    return (sum(Qc)/N_avg, sum(Qf)/N_avg)
